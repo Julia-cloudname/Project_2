faf6fdcbbe4ce9fa1a3dda51c933f386b2ce17f8
Julia <malukjulia2014@gmail.com>
2023-02-10 20:46:36 +0000

    Add functions for start timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,39 @@ function checkTime(time) {
       return time;
     }
   }
+
+  /**
+ * The function updates the inner text of div elements with the current value of time. 
+ */  
+function displayTime() {
+    hourDiv.innerText = checkTime(hours);
+    minuteDiv.innerText = checkTime(minutes);
+    secondDiv.innerText = checkTime(seconds);
+    millisecondDiv.innerText = checkTime(milliseconds);
+  }
+ /**
+  * The function starts a timer that increments milliseconds every 10 Msec.
+  *  When the Msec reach 100, the seconds are incremented and the Msec 
+  * are reset to 0. Similar checks are made for minutes and hours.
+  */ 
+function startTimer() {
+interval = setInterval(() => {
+milliseconds++;
+
+    if (milliseconds === 100) {
+        seconds++;
+        milliseconds = 0;
+    }
+    if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+    }
+    if (minutes === 60) {
+        hours++;
+        minutes = 0;
+    }
+
+    displayTime()
+}, 10);
+}
+
