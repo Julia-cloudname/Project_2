d267bdeef83fc8917cbe9208b68ad5045837be75
Julia <malukjulia2014@gmail.com>
2023-02-12 19:17:44 +0000

    Add pause button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const minuteDiv = document.querySelector('.minute');
   const secondDiv = document.querySelector('.second');
   const millisecondDiv = document.querySelector('.millisecond');
+
+  let hours = 0, minutes = 0, seconds = 0, milliseconds = 0, interval;
+
+
+  //Buttons
   const startButton = document.querySelector('.start');
   const stopButton = document.querySelector('.stop');
   const newTaskButton = document.querySelector('.newTask');
 
-  let hours = 0, minutes = 0, seconds = 0, milliseconds = 0, interval;
+  //Hide buttons
+
+  let pauseButton;
 
   /**
    * This function takes a time as parameter and checks if it is less than 10.
@@ -33,13 +40,24 @@ document.addEventListener("DOMContentLoaded", () => {
       secondDiv.innerText = checkTime(seconds);
       millisecondDiv.innerText = checkTime(milliseconds);
   }
-
+  
+  /**
+   * Create pauseButton, set Start Button in it and update content and style of Start Button
+   */
+  function updateStartButton() {
+    pauseButton = startButton;
+    pauseButton.id = 'pause';
+    pauseButton.innerHTML = "<p2>Pause</p2>"
+    pauseButton.style.backgroundColor = "yellow";
+  }
   /**
    * The function starts a timer that increments milliseconds.
    *  When the Msec reach 100, the seconds are incremented and the Msec
    * are reset to 0. Similar checks are mades for minutes and hours.
    */
   function startTimer() {
+    
+    updateStartButton();
       return setInterval(
           () => {
               milliseconds++;
@@ -58,8 +76,10 @@ document.addEventListener("DOMContentLoaded", () => {
               }
 
               displayTime()
-          }, 10);
+          }, 10);        
   }
+
+  
 
   startButton.addEventListener('click', () => {
       clearInterval(interval);
@@ -67,10 +87,22 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   stopButton.addEventListener('click', () => {
       clearInterval(interval);
+      
+      hours = 0, minutes = 0, seconds = 0, milliseconds = 0, interval;
+      
+      millisecondDiv.textContent = "00" 
+      secondDiv.textContent = "00" 
+      minuteDiv.textContent = "00" 
+      hourDiv.textContent = "00" 
   });
 
-  newTaskButton.addEventListener('click', () => {
-    interval = startTimer(); 
-  });
+ let resumeButton = document.getElementById('pause');
+     resumeButton.addEventListener ('click', () => {
+     clearInterval(interval);
+     resumeButton.innerHTML = "<p2>Resume</p2>"
+     resumeButton.style.backgroundColor = "Green";
+});
+
+
 
 });
