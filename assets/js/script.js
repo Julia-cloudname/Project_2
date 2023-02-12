30c0518fb998197d32f01b0dc65c07133784a0f8
Julia <malukjulia2014@gmail.com>
2023-02-12 16:19:08 +0000

    Create template for new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  /**Area displays time */
+  /**Area displays time and buttons*/
   const hourDiv = document.querySelector('.hour');
   const minuteDiv = document.querySelector('.minute');
   const secondDiv = document.querySelector('.second');
   const millisecondDiv = document.querySelector('.millisecond');
   const startButton = document.querySelector('.start');
   const stopButton = document.querySelector('.stop');
+  const newTaskButton = document.querySelector('.newTask');
 
   let hours = 0, minutes = 0, seconds = 0, milliseconds = 0, interval;
 
@@ -67,4 +68,9 @@ document.addEventListener("DOMContentLoaded", () => {
   stopButton.addEventListener('click', () => {
       clearInterval(interval);
   });
+
+  newTaskButton.addEventListener('click', () => {
+    interval = startTimer(); 
+  });
+
 });
