fbc0fef5dfd06988bf5fcfd587c92468ec6c74f0
Julia <malukjulia2014@gmail.com>
2023-02-13 00:16:20 +0000

    Add addEventListeners and add function for start and stop timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const secondDiv = document.querySelector('.second');
   const millisecondDiv = document.querySelector('.millisecond');
 
-  let hours = 0, minutes = 0, seconds = 0, milliseconds = 0, interval;
+  let hours = 0,
+    minutes = 0,
+    seconds = 0,
+    milliseconds = 0,
+    timerId;
+
+  let paused = true;  
 
 
   //Buttons
-  const startButton = document.querySelector('.start');
+  const startPauseButton = document.querySelector('#start-pause');
   const stopButton = document.querySelector('.stop');
   const newTaskButton = document.querySelector('.newTask');
 
   //Hide buttons
 
-  let pauseButton;
+
 
   /**
    * This function takes a time as parameter and checks if it is less than 10.
@@ -24,84 +30,114 @@ document.addEventListener("DOMContentLoaded", () => {
    * if the time value is 10 or greater.
    */
   function checkTime(time) {
-      if (time < 10) {
-          return "0" + time;
-      } else {
-          return time;
-      }
+    if (time < 10) {
+      return "0" + time;
+    } else {
+      return time;
+    }
   }
 
   /**
    * The function updates the inner text of div elements with the current value of time.
    */
   function displayTime() {
-      hourDiv.innerText = checkTime(hours);
-      minuteDiv.innerText = checkTime(minutes);
-      secondDiv.innerText = checkTime(seconds);
-      millisecondDiv.innerText = checkTime(milliseconds);
+    hourDiv.innerText = checkTime(hours);
+    minuteDiv.innerText = checkTime(minutes);
+    secondDiv.innerText = checkTime(seconds);
+    millisecondDiv.innerText = checkTime(milliseconds);
   }
-  
-  /**
-   * Create pauseButton, set Start Button in it and update content and style of Start Button
-   */
-  function updateStartButton() {
-    pauseButton = startButton;
-    pauseButton.id = 'pause';
-    pauseButton.innerHTML = "<p2>Pause</p2>"
-    pauseButton.style.backgroundColor = "yellow";
-  }
+
+
   /**
    * The function starts a timer that increments milliseconds.
    *  When the Msec reach 100, the seconds are incremented and the Msec
    * are reset to 0. Similar checks are mades for minutes and hours.
    */
   function startTimer() {
-    
-    updateStartButton();
-      return setInterval(
-          () => {
-              milliseconds++;
+    paused = false;
+    return setInterval(
+      () => {
+        milliseconds++;
 
-              if (milliseconds === 100) {
-                  seconds++;
-                  milliseconds = 0;
-              }
-              if (seconds === 60) {
-                  minutes++;
-                  seconds = 0;
-              }
-              if (minutes === 60) {
-                  hours++;
-                  minutes = 0;
-              }
+        if (milliseconds === 100) {
+          seconds++;
+          milliseconds = 0;
+        }
+        if (seconds === 60) {
+          minutes++;
+          seconds = 0;
+        }
+        if (minutes === 60) {
+          hours++;
+          minutes = 0;
+        }
 
-              displayTime()
-          }, 10);        
+        displayTime()
+      }, 10);
   }
 
-  
+  function pauseTimer(timerId) {
+    clearInterval(timerId);
+    paused = true;
 
-  startButton.addEventListener('click', () => {
-      clearInterval(interval);
-      interval = startTimer();
+  }
+
+  function stopTimer(timerId) {
+    clearInterval(timerId);
+    paused = true;
+
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    return null;
+  }
+
+  /**
+   * Create pauseButton, set Start Button in it and update content and style of Start Button
+   */
+  function updateStartButton() {
+
+    startPauseButton.class = 'pause';
+    startPauseButton.innerHTML = "<p2>Pause</p2>"
+    startPauseButton.style.backgroundColor = "yellow";
+  }
+
+  function updatePauseButton() {
+
+    startPauseButton.class = 'resume';
+    startPauseButton.innerHTML = "<p2>Resume</p2>"
+    startPauseButton.style.backgroundColor = "green";
+
+  }
+
+
+
+  //Listeners
+
+  startPauseButton.addEventListener('click', (event) => {
+    let btn = event.target;
+    if (paused) {
+      timerId = startTimer();
+      btn.textContent = 'Pause';
+    } else {
+      pauseTimer(timerId);
+      btn.textContent = 'Resume';
+    }
+    btn.classList.toggle('start');
+    btn.classList.toggle('pause');
+
   });
+
   stopButton.addEventListener('click', () => {
-      clearInterval(interval);
-      
-      hours = 0, minutes = 0, seconds = 0, milliseconds = 0, interval;
-      
-      millisecondDiv.textContent = "00" 
-      secondDiv.textContent = "00" 
-      minuteDiv.textContent = "00" 
-      hourDiv.textContent = "00" 
+
+
+    millisecondDiv.textContent = "00"
+    secondDiv.textContent = "00"
+    minuteDiv.textContent = "00"
+    hourDiv.textContent = "00"
   });
 
- let resumeButton = document.getElementById('pause');
-     resumeButton.addEventListener ('click', () => {
-     clearInterval(interval);
-     resumeButton.innerHTML = "<p2>Resume</p2>"
-     resumeButton.style.backgroundColor = "Green";
-});
 
 
 
