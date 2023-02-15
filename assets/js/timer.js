42b864dd62e2b804b80eb13fe0b1c4a2afba5462
Julia <malukjulia2014@gmail.com>
2023-02-15 21:59:41 +0000

    Add function for start, stop and clear timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,9 +1,14 @@
+// Variables for timer
 const timerTickIntervalMs = 10;
 let timerMilliseconds = 0;
 let timerSavedMilliseconds = 0;
 let timerId = null;
 let timerOnTick = null;
 
+/**
+ * The function increments the timer by the timerTickIntervalMs value and calls 
+ * a callback function (timerOnTick) with the updated timer value as a parameter.
+ */
 function timerTick() {
     timerMilliseconds += timerTickIntervalMs;
     if (timerOnTick != null) {
@@ -11,26 +16,43 @@ function timerTick() {
     }
 }
 
+/**
+ * Function clears any existing timer and starts a new one with the timerTick 
+ * function as a callback and interval timerTickIntervalMs.
+ */ 
 function timerStart () {
     clearInterval(timerId);
     timerId = setInterval(timerTick, timerTickIntervalMs);
     console.log('started', timerId);
 }
 
+/**
+ * Function stops the timer and clears the timer interval.
+ */
 function timerStop() {
     clearInterval(timerId);
     timerId = null;
     console.log('stoped', timerId);
 }
 
+/**
+ * Function checks if the timer is currently running and @returns a boolean value
+ */
 function isTimerStarted() {
     return timerId != null;
 }
 
+/**
+ * Function resets the timer to zero Ms
+ */
 function timerClear() {
     timerMilliseconds = 0;
 }
 
+/**
+ * Function calculates the elapsed time between the last two laps of the timer 
+ * (i.e., the time between the last two calls to this function). @returns calculation result
+ */
 function timerLastLap() {
     let lastLapMs = timerMilliseconds - timerSavedMilliseconds;
     timerSavedMilliseconds = timerMilliseconds;
@@ -38,6 +60,9 @@ function timerLastLap() {
     return lastLapMs;
 }
 
+/**
+ * Function sets a callback function @param {*} onTick to be called on every tick of the timer.
+ */
 function setOnTick(onTick) {
     timerOnTick = onTick;
 }
